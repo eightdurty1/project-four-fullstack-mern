7dd3c40d58ce045da3af3bb03b647da4de692367
Israel Flores <israelflo92@gmail.com>
2022-05-25 10:32:13 -0700

    create profile page and singlepostpage and profilebio

---
+++ src/components/ProfileBio/ProfileBio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProfileBio() {
+    return(
+        <h1>This is the Profile Bio</h1>
+    )
+}
+
+export default ProfileBio
---
+++ src/pages/SinglePostPage/SinglePostPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SinglePostPage(){
+    return (
+        <h1>This is the single post page.</h1>
+    )
+}
+
+export default SinglePostPage
